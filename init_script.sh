eb4ac87603c2fa8334ae2f63d84cdf92f2e1b2a3
Manu Joseph <manujosephv@gmail.com>
2023-05-27 05:42:53 +0000

    first commit

---
+++ init_script.sh
@@ -0,0 +1,18 @@
+DIR="GATE_v2"
+if [ -d "$DIR" ]; then
+  ### Take action if $DIR exists ###
+  echo "Pulling Latest Changes"
+  cd $DIR
+  git pull origin
+else
+  ###  Control will jump here if $DIR does NOT exists ###
+  echo "Error: ${DIR} not found. Cloning it from source."
+  git clone https://github.com/manujosephv/GATE_v2
+  cd $DIR
+fi
+
+echo "Installing PyTorch Tabular with GATE"
+pip install .
+cd ..
+echo "Installing Other Requirements"
+pip install -r requirements.txt
