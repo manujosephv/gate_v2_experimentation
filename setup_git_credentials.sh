5ca6f0cb15811a060f7e4eba48976812fdb3ef2a
Manu Joseph <manujosephv@gmail.com>
2023-05-28 08:32:12 +0530

    enhanced gitignore

--- setup_git_credentials.sh
+++ setup_git_credentials.sh
@@ -2,4 +2,4 @@ git config --global credential.helper "/bin/bash /git_creds.sh"
 echo '#!/bin/bash' > /git_creds.sh
 echo "sleep 1" >> /git_creds.sh
 echo "echo username=manujosephv" >> /git_creds.sh
-echo "echo password=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" >> /git_creds.sh
+echo "echo password=<<PAT>>" >> /git_creds.sh
